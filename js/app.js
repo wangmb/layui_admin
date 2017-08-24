317d88f8fbc4c6af9470d87ef18f7c12489e1918
Van <zheng_jinfan@126.com>
2017-08-24 17:10:34 +0800

    第一次提交

---
+++ js/app.js
@@ -0,0 +1,80 @@
+layui.define(['element', 'nprogress', 'form', 'table', 'loader', 'iframe', 'navbar'], function(exports) {
+    var $ = layui.jquery,
+        element = layui.element,
+        layer = layui.layer,
+        _win = $(window),
+        _doc = $(document),
+        _body = $('.kit-body'),
+        form = layui.form,
+        table = layui.table,
+        loader = layui.loader,
+        iframe = layui.iframe,
+        navbar = layui.navbar,
+        _componentPath = 'components/';
+    var app = {
+        hello: function(str) {
+            layer.alert('Hello ' + (str || 'test'));
+        },
+        config: {
+            type: 'iframe'
+        },
+        set: function(options) {
+            var that = this;
+            $.extend(true, that.config, options);
+            return that;
+        },
+        init: function() {
+            var that = this,
+                _config = that.config;
+            if (_config.type === 'page') {
+                $('a[kit-target]').on('click', function() {
+                    var url = $(this).data('url'),
+                        name = $(this).data('name'),
+                        id = $(this).data('id');
+                    loader.load({
+                        url: url,
+                        name: name,
+                        id: id === undefined ? new Date().getTime() : id,
+                        onSuccess: success
+                    });
+
+                    function success(data) {
+                        switch (data.name) {
+                            case 'table':
+                                loader.getScript(_componentPath + 'table/table.js', function() {
+                                    var tableIns = table.render(moduleTable.config);
+                                    moduleTable.extend({
+                                        currTable: tableIns,
+                                        table: table,
+                                        layer: layer,
+                                        form: form,
+                                        jquery: $
+                                    });
+                                });
+                                break;
+                            case 'form':
+                                form.render();
+                                break;
+                            default:
+                                break;
+                        }
+                    };
+                });
+            }
+            if (_config.type === 'iframe') {
+                var ifr = new iframe().set({ elem: '#container' }).render();
+                //navbar加载方式一，直接绑定已有的dom元素事件
+                navbar.bind(function(data) {
+                    ifr.tabAdd(data);
+                });
+            }
+
+            return that;
+        }
+    };
+    // app.hello();
+    //app.init();
+
+    //输出test接口
+    exports('app', app);
+});
---
+++ js/navbar.js
@@ -0,0 +1,59 @@
+layui.define(['jquery'], function(exports) {
+    var $ = layui.jquery,
+        _modName = 'navbar',
+        _win = $(window),
+        _doc = $(document);
+
+    var navbar = {
+        config: {
+            elem: undefined,
+            filter: 'kitNavbar' //过滤器名称
+        },
+        set: function(options) {
+            var that = this;
+            $.extend(true, that.config, options);
+            return that;
+        },
+        /**
+         * 绑定特定a标签的点击事件
+         */
+        bind: function(callback) {
+            var that = this,
+                _config = that.config;
+            if (_config.elem === undefined && _doc.find('ul[kit-navbar]').length === 0) {
+                layui.hint().error('Navbar error:请配置Navbar容器.');
+                return that;
+            }
+            var _elem;
+            if (_config.elem !== undefined && $(_config.elem).length > 0) {
+                _elem = $(_config.elem);
+            } else {
+                _elem = _doc.find('ul[kit-navbar]');
+            }
+            _elem.find('a[kit-target]').each(function() {
+                var _that = $(this);
+                _that.off('click').on('click', function() {
+                    var options = _that.data('options');
+                    var data;
+                    if (options !== undefined) {
+                        try {
+                            data = new Function('return ' + options)();
+                        } catch (e) {
+                            layui.hint().error('Navbar 组件a[data-options]配置项存在语法错误：' + options)
+                        }
+                    } else {
+                        data = {
+                            icon: _that.data('icon'),
+                            id: _that.data('id'),
+                            title: _that.data('title'),
+                            url: _that.data('url'),
+                        };
+                    }
+                    typeof callback === 'function' && callback(data);
+                });
+            });
+            return that;
+        }
+    };
+    exports('navbar', navbar);
+});
