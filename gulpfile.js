8a779505d6d8cf2bc749d8e360e8b924b6fa514f
Van <zheng_jinfan@126.com>
2017-11-07 16:18:26 +0800

    layui更新到2.1.7，开放src源代码.

--- gulpfile.js
+++ gulpfile.js
@@ -12,23 +12,23 @@ var minimist = require('minimist');
 var zip = require('gulp-zip');
 
 var task = {
-    mincss: function() {
-        return gulp.src(['./src/css/**/*.css'])
-            .pipe(minify())
-            .pipe(header.apply(null, ['/** <%= pkg.name %>-v<%= pkg.version %> <%= pkg.license %> License By <%= pkg.homepage %> e-mail:<%= pkg.email %> */\n', { pkg: pkg }]))
-            .pipe(gulp.dest('./build/css'));
-    },
-    minjs: function() {
-        return gulp.src('./src/js/*.js')
-            .pipe(gulpif(['!app.js', '!config.js'], uglify()))
-            .pipe(header.apply(null, ['/** <%= pkg.name %>-v<%= pkg.version %> <%= pkg.license %> License By <%= pkg.homepage %> e-mail:<%= pkg.email %> */\n <%= js %>', { pkg: pkg, js: ';' }]))
-            .pipe(gulp.dest('./build/js'));
-    },
-    file: function() {
-        return gulp.src(['./src/images/**/*.{png,jpg,gif,html,mp3,json}'])
-            .pipe(rename({}))
-            .pipe(gulp.dest('./build/images'));
-    }
+  mincss: function() {
+    return gulp.src(['./src/css/**/*.css'])
+      .pipe(minify())
+      .pipe(header.apply(null, ['/** <%= pkg.name %>-v<%= pkg.version %> <%= pkg.license %> License By <%= pkg.homepage %> e-mail:<%= pkg.email %> */\n', { pkg: pkg }]))
+      .pipe(gulp.dest('./build/css'));
+  },
+  minjs: function() {
+    return gulp.src('./src/js/*.js')
+      .pipe(gulpif(['!app.js', '!config.js'], uglify()))
+      .pipe(header.apply(null, ['/** <%= pkg.name %>-v<%= pkg.version %> <%= pkg.license %> License By <%= pkg.homepage %> e-mail:<%= pkg.email %> */\n <%= js %>', { pkg: pkg, js: ';' }]))
+      .pipe(gulp.dest('./build/js'));
+  },
+  file: function() {
+    return gulp.src(['./src/images/**/*.{png,jpg,gif,html,mp3,json}'])
+      .pipe(rename({}))
+      .pipe(gulp.dest('./build/images'));
+  }
 };
 gulp.task('minjs', task.minjs);
 gulp.task('mincss', task.mincss);
@@ -37,10 +37,10 @@ gulp.task('default', function() {
 
 });
 gulp.task('all', ['clear'], function() {
-    for (var key in task) {
-        task[key]();
-    }
+  for (var key in task) {
+    task[key]();
+  }
 });
 gulp.task('clear', function(cb) {
-    return del(['./build/*'], cb);
+  return del(['./build/*'], cb);
 });
