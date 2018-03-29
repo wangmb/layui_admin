75cec72063dfeb72e69f021b4ea5a2bf2220d187
Van <zheng_jinfan@126.com>
2018-03-29 11:41:34 +0800

    #layui 更新到2.2.5 #修改左侧菜单的一个bug #添加新版登录页面 http://kit.zhengjinfan.cn/login.html

--- src/js/app.js
+++ src/js/app.js
@@ -194,29 +194,29 @@ layui.define(['element', 'nprogress', 'form', 'table', 'loader', 'tab', 'navbar'
                 }
             }
 
-            // ripple start
-            var addRippleEffect = function(e) {
-                // console.log(e);
-                layui.stope(e)
-                var target = e.target;
-                if (target.localName !== 'button' && target.localName !== 'a') return false;
-                var rect = target.getBoundingClientRect();
-                var ripple = target.querySelector('.ripple');
-                if (!ripple) {
-                    ripple = document.createElement('span');
-                    ripple.className = 'ripple'
-                    ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px'
-                    target.appendChild(ripple);
-                }
-                ripple.classList.remove('show');
-                var top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
-                var left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
-                ripple.style.top = top + 'px'
-                ripple.style.left = left + 'px'
-                ripple.classList.add('show');
-                return false;
-            }
-            document.addEventListener('click', addRippleEffect, false);
+            // // ripple start
+            // var addRippleEffect = function(e) {
+            //     // console.log(e);
+            //     layui.stope(e)
+            //     var target = e.target;
+            //     if (target.localName !== 'button' && target.localName !== 'a') return false;
+            //     var rect = target.getBoundingClientRect();
+            //     var ripple = target.querySelector('.ripple');
+            //     if (!ripple) {
+            //         ripple = document.createElement('span');
+            //         ripple.className = 'ripple'
+            //         ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px'
+            //         target.appendChild(ripple);
+            //     }
+            //     ripple.classList.remove('show');
+            //     var top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
+            //     var left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
+            //     ripple.style.top = top + 'px'
+            //     ripple.style.left = left + 'px'
+            //     ripple.classList.add('show');
+            //     return false;
+            // }
+            // document.addEventListener('click', addRippleEffect, false);
             // ripple end
 
             return that;
